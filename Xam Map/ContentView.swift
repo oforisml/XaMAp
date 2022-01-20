bc6a2778feeb3ad5f648d5f077e7ae5fb65fed0c
Samuel Ofori <sofori006@stu.ucc.edu.gh>
2022-01-20 15:23:59 +0000

    Initial Commit

---
+++ Xam Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xam Map
+//
+//  Created by Samuel Ofori on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xam Map/Xam_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xam_MapApp.swift
+//  Xam Map
+//
+//  Created by Samuel Ofori on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Xam_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
